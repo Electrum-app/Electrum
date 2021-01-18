13eea3953df5c5c3e6a6122adbbf57f84e7908ab
j-berg <j-berg@users.noreply.github.com>
2021-01-18 16:11:39 -0700

    Update

--- bin/run.sh
+++ bin/run.sh
@@ -25,10 +25,12 @@ nvidia-smi -L
 # conda install -c nvidia -c rapidsai -c numba -c conda-forge -c defaults cugraph cudatoolkit=11.0
 # conda install python networkx numpy pandas requests pip
 # python -m pip install pysmiles
+echo "+ Activating conda environment..."
 source /uufs/chpc.utah.edu/common/home/u0690617/miniconda3/etc/profile.d/conda.sh
 source activate substructure
 
 # Set instance variables
+echo "+ Setting environment..."
 HOME=/uufs/chpc.utah.edu/common/home/u0690617
 SCRDIR=/scratch/general/lustre/$USER/$SLURM_JOBID
 mkdir -p $SCRDIR
@@ -36,6 +38,7 @@ cd $SCRDIR
 mkdir -p $SCRDIR/output
 
 # Get main script
+echo "+ Copying scripts..."
 mkdir -p $SCRDIR/bin
 cp $HOME/programs/Electrum/bin/__main__.py $SCRDIR/bin
 
@@ -43,4 +46,7 @@ cp $HOME/programs/Electrum/bin/__main__.py $SCRDIR/bin
 cd $SCRDIR
 
 # Run python script
+echo "+ Running scripts..."
 python $SCRDIR/bin/__main__.py $SCRDIR/output
+
+echo "+ Processing complete..."
